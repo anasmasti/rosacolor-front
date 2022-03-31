54ef5b603fbbf0a65c19220acbc5993a992bdf60
anasmasti <anasmasti@hotmail.com>
2022-03-31 16:29:53 +0000

    Fix contact page UI issues

--- components/contact/RightSecion.jsx
+++ components/contact/RightSecion.jsx
@@ -2,7 +2,7 @@ import ContactForm from "./ContactForm";
 
 export default function RightSecion() {
   return (
-    <div className="w-full lg:w-1/2 xl:w-5/12 px-4">
+    <div className="w-full lg:w-1/2 xl:w-5/12 px-4 xl:mt-20 lg:mt-16">
       <div className="bg-red-500 relative rounded-lg p-8 sm:p-12 shadow-lg">
         <ContactForm />
       </div>
--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -3,7 +3,7 @@ import LeftSection from "../../components/contact/LeftSection";
 
 export default function contact() {
   return (
-    <section className="bg-white py-20 xl:px-20  px-11 lg:py-[120px] overflow-hidden relative z-10">
+    <section className="bg-white py-20 px-6 overflow-hidden relative z-10 flex justify-center items-center">
       <div className="container">
         <div className="flex flex-wrap lg:justify-between -mx-4">
           <LeftSection />
