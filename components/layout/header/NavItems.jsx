364811f04dd990349706d0249580540983d9edba
anasmasti <anasmasti@hotmail.com>
2022-03-31 09:20:02 +0000

    Add Loayout components
    - Add NavItem
    - Add NavItems and list of menu items
    - Add Navbar
    - Add Footer
    - Add Copyright

---
+++ components/layout/header/NavItems.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function NavItems() {
+  let menuItems = [
+    { id: 1, title: "Accueil", href: "home" },
+    { id: 1, title: "À propos de nous", href: "about" },
+    { id: 1, title: "Produits", href: "products" },
+    { id: 1, title: "Contact", href: "contact" },
+  ];
+  return (<>
+  NavItems
+  </>);
+}
