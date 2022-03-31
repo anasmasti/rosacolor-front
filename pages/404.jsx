b5a8547c0295674bebe03920e87da82d9dea8469
anasmasti <anasmasti@hotmail.com>
2022-03-31 13:51:39 +0000

    Add contact and about page

---
+++ pages/404.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="h-screen w-full flex flex-col justify-center items-center bg-white">
+      <h1 className="text-9xl font-extrabold text-neutral-900 tracking-widest">
+        404
+      </h1>
+      <div className="bg-red-500 text-neutral-100 px-2 text-sm rounded rotate-12 absolute">
+        Page Not Found
+      </div>
+      <div className="mt-5">
+        <Link href="/">
+          <a className="w-full text-white bg-red-500 rounded border border-primary p-3 transition hover:bg-opacity-90">
+            Aller à la page d'accueil
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
+}
--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,5 +1,43 @@
+import ContactForm from "../../components/contact/ContactForm";
+
 export default function contact() {
   return (
-    <div>contact</div>
-  )
+    <section className="bg-white py-20 xl:px-20  px-11 lg:py-[120px] overflow-hidden relative z-10">
+      <div className="container">
+        <div className="flex flex-wrap lg:justify-between -mx-4">
+          <div className="w-full lg:w-1/2 xl:w-6/12 px-4">
+            <div className="max-w-[570px] mb-12 lg:mb-0">
+              <span className="block mb-4 text-base text-primary font-semibold">
+                Contactez-nous
+              </span>
+              <h2
+                className="
+                  text-dark
+                  mb-6
+                  uppercase
+                  font-bold
+                  text-[32px]
+                  sm:text-[40px]
+                  lg:text-[36px]
+                  xl:text-5xl
+                  "
+              >
+                PRENEZ CONTACT AVEC NOUS
+              </h2>
+              <p className="text-base text-body-color leading-relaxed mb-9">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eius tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                adiqua minim veniam quis nostrud exercitation ullamco
+              </p>
+            </div>
+          </div>
+          <div className="w-full lg:w-1/2 xl:w-5/12 px-4">
+            <div className="bg-red-500 relative rounded-lg p-8 sm:p-12 shadow-lg">
+              <ContactForm />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
