4e137c65fe4d03bf91e18f6a43096d8b6c51389d
anasmasti <anasmasti@hotmail.com>
2022-04-01 12:13:16 +0000

    Refactor home page code

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -6,7 +6,6 @@ export default function Home() {
     <div className="relative items-center justify-center w-full overflow-x-hidden lg:pt-40 lg:pb-40 xl:pt-40 xl:pb-64">
       <div className="container flex flex-col items-center justify-between h-full max-w-6xl px-8 mx-auto -mt-32 lg:flex-row xl:px-0">
         <LeftSection />
-
         <RightSection />
       </div>
     </div>
--- components/home/landing/MainParagraph.jsx
+++ components/home/landing/MainParagraph.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function MainParagraph() {
   return (
     <p className="pr-0 mb-8 text-base text-neutral-400 sm:text-lg xl:text-xl lg:pr-20">
