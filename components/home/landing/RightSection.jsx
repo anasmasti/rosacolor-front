1c00820112521135b7ea681551a9570deb029bb4
anasmasti <anasmasti@hotmail.com>
2022-04-01 12:39:28 +0000

    Fix landing right section UI issues

--- components/home/landing/RightSection.jsx
+++ components/home/landing/RightSection.jsx
@@ -4,7 +4,7 @@ export default function RightSection() {
       <div className="container relative left-0 w-full max-w-4xl lg:absolute xl:max-w-6xl lg:w-screen">
         <img
           src="https://images.pexels.com/photos/6757565/pexels-photo-6757565.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          className="h-screen w-full"
+          className="h-screen w-full hidden lg:block xl:block"
         />
       </div>
     </div>
