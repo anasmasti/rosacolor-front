2fcd43d3063f87f380f945458e7207a05c14a931
anasmasti <anasmasti@hotmail.com>
2022-04-04 15:14:40 +0000

    Add Image component

--- components/home/landing/RightSection.jsx
+++ components/home/landing/RightSection.jsx
@@ -1,12 +1,23 @@
+import Image from "next/image";
+import landingMainImage from "../../../public/images/landing/main.jpeg";
+// import style from "../../../public/styles/main-image.css";
+
 export default function RightSection() {
+  // console.log(style);
   return (
-    <div className="relative z-50 flex flex-col items-end justify-center w-full lg:w-1/2 ">
-      <div className="container relative left-0 w-full max-w-4xl lg:absolute xl:max-w-6xl lg:w-screen">
-        <img
-          src="https://images.pexels.com/photos/6757565/pexels-photo-6757565.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          className="h-screen w-full hidden lg:block xl:block"
-        />
+      <div className="relative z-50 flex flex-col items-end justify-center w-full lg:w-1/2">
+        <div
+          className={"container relative left-0 w-full max-w-4xl lg:absolute xl:max-w-6xl lg:w-screen"}
+        >
+          <Image
+            src={landingMainImage}
+            height="1000"
+            loading="eager"
+            alt="Rosacolor landing image"
+            id="landing-image"
+            quality={50}
+          />
+        </div>
       </div>
-    </div>
   );
 }
