a2e1d30987cd3d04d47b2ff4cb8ee253d4a2beae
YassineAm00 <yassine.19am49@gmail.com>
2022-07-08 17:51:38 +0100

    add json data for products

--- components/products/productdetails/ProductDetailsItems.jsx
+++ components/products/productdetails/ProductDetailsItems.jsx
@@ -1,14 +1,7 @@
 import React from 'react'
 
 export default function ProductDetailsItems() {
-    const listProduct= [
-      {
-        id:1,
-        title:"",
-        description:"",
-        src:""
-      },
-    ]
+
   return (
     <div>ProductDetailsItems</div>
   )
